06563aac82001e8a5bc4105383be97f0dceb8be9
Gian Vallejos <gian.vallejos92@gmail.com>
2017-12-27 00:24:31 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default{
+  appname: 'platzimusic',
+  apiKey: 'cbd9d09abebe39adedbf8181366fb80a',
+  secret: 'e81e2fd97bad20f6d30a42ed267c400e',
+  registeredTo: 'gvallejos92'
+}
---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+// Encargado de hacer request al apiKey
+import config from './config.js'
+
+const { apiKey } = config
+const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${ apiKey }&format=json`;
+
+export default function getArtists(country){
+  const url = URL.replace(':country', country)
+  return fetch(url)
+          .then(res => res.json())
+          .then(json => json.topartists.artist)
+
+}
